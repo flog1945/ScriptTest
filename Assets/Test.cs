bebcfc22ca19eeb4fec828755c5edfb0b730ab7e
坂田 玄樹 <ginga777@cnc.bbiq.jp>
2018-01-25 19:42:53 +0900

    ScriptTestプロジェクト課題 (再提出分)
    発展課題の再提出分となります。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,26 +6,24 @@ public class Boss
 {
     //発展課題
 
+    //int型の変数mpを宣言し、53で初期化してください
+    int mp = 53;
     //mpを消費して魔法攻撃をするMagic関数を作ってください
     public void Magic()
     {
-        //int型の変数mpを宣言し、53で初期化してください
-        int mp = 53;
         //Magic関数内でmpを5減らし、コンソールに「魔法攻撃をした。残りMPは〇〇。」と表示してください。mpが足りない場合、「MPが足りないため魔法が使えない。」と表示してください
-        for(; mp>=0;)
-        {
-            mp -= 5;
-            if ( mp >= 0)
-            { 
+            if ( mp >= 5)
+            {
+                mp -= 5;
                 Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
             }
             else
             {
                 Debug.Log("MPが足りないため魔法が使えない。");
             }
-        }
     }
 }
+
 
 
 public class Test : MonoBehaviour
@@ -47,7 +45,8 @@ public class Test : MonoBehaviour
 
      //発展課題
      Boss boss = new Boss();
-     boss.Magic();
+        for(int i = 0; i < 10; i++) { boss.Magic(); }
+        boss.Magic();
     }
 
     // Update is called once per frame
